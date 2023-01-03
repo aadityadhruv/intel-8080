f346b5978aee2df533cb557140ea1caf8f4a1875
Aaditya Dhruv <aadityadhruv@mailbox.org>
2023-01-03 16:49:22 +0530

    basic cmd line parsing using clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 mod debug;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Set this flag to run the ROM under debug mode 
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug : u8,
+    /// The path to the ROM, e.g. /path/to/rom
+    rom : String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let rom = args.rom;
+    let debug = args.debug > 0;
+    println!("Debug mode is {}, loading ROM {}", if debug {"ON"} else {"OFF"}, rom);
 }
